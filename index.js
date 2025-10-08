ffde335de3ca9292372b518a0a064c93f7004720
achibhossengit <mail.achibhossen@gmail.com>
2025-10-08 10:43:01 +0600

    Secure API routes by verifying Firebase ID tokens via Admin SDK

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +15,37 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyFirebaseToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Missing or malformed token" });
+  }
+
+  const accessToken = authHeader.split(" ")[1];
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(accessToken);
+    // console.log(decodedToken);
+    req.user = decodedToken;
+  } catch (error) {
+    console.error("Token verification Failed: ", error);
+    return res.status(403).json({ error: "Invalid Token" });
+  }
+
+  next();
+};
+
+// Utility Function
+const getUserEmail = (req, res) => {
+  const email = req.user?.email;
+  if (!email) {
+    res.status(403).json({ error: "User email not found!" });
+    throw new Error("Email missing");
+  }
+  return email;
+};
+
 
 // MongoDB connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2dlckac.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -32,19 +69,22 @@ async function run() {
     });
 
     // parcel related api
-    app.get("/parcels", async (req, res) => {
+    app.get("/parcels", verifyFirebaseToken, async (req, res) => {
       try {
-        const parcels = await parcelsColl.find().toArray();
-        res.status(200).send(parcels);
+        const email = getUserEmail(req, res)
+        const query = { created_by: email };
+        const parcels = await parcelsColl.find(query).toArray();
+        return res.status(200).send(parcels);
       } catch (err) {
-        res.status(500).send({ error: "Failed to fetch parcels" });
+        return res.status(500).send({ error: "Failed to fetch parcels" });
       }
     });
 
-    app.get("/parcels/:id", async (req, res) => {
+    app.get("/parcels/:id", verifyFirebaseToken, async (req, res) => {
       try {
+        const email = getUserEmail(req, res)
         const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
+        const query = { _id: new ObjectId(id), created_by: email };
         const result = await parcelsColl.findOne(query);
         return res.send(result);
       } catch (error) {
@@ -52,9 +92,13 @@ async function run() {
       }
     });
 
-    app.post("/parcels", async (req, res) => {
+    app.post("/parcels", verifyFirebaseToken, async (req, res) => {
       try {
-        const parcelData = req.body;
+        const email = getUserEmail(req, res)
+        const parcelData = {
+          ...req.body,
+          created_by: email,
+        };
 
         const result = await parcelsColl.insertOne(parcelData);
         res.status(201).send(result);
@@ -63,12 +107,13 @@ async function run() {
       }
     });
 
-    app.put("/parcels/:id", async (req, res) => {
+    app.put("/parcels/:id", verifyFirebaseToken, async (req, res) => {
       try {
+        const email = getUserEmail(req, res)
         const id = req.params.id;
         const updatedData = req.body;
         delete updatedData._id;
-        const query = { _id: new ObjectId(id) };
+        const query = { _id: new ObjectId(id), created_by: email };
         const result = await parcelsColl.updateOne(query, {
           $set: updatedData,
         });
@@ -79,10 +124,11 @@ async function run() {
       }
     });
 
-    app.delete("/parcels/:id", async (req, res) => {
+    app.delete("/parcels/:id", verifyFirebaseToken, async (req, res) => {
       try {
+        const email = getUserEmail(req, res)
         const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
+        const query = { _id: new ObjectId(id), created_by: email };
         const result = await parcelsColl.deleteOne(query);
         return res.send(result);
       } catch (error) {
